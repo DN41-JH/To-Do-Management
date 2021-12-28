b6052efe5dc19dd5e3216722eb30e154537b8589
JianghuaiL <77869949+DN41-JH@users.noreply.github.com>
2021-12-28 15:04:47 -0600

    Update ToDoList.jsx

--- todomanagement_frontend/src/components/ToDoList.jsx
+++ todomanagement_frontend/src/components/ToDoList.jsx
@@ -18,30 +18,34 @@ export default class ToDoList extends React.Component {
         return (
             <div>
                 <h1>List of Todos</h1>
-                <table>
-                    <thead>
-                        <tr>
-                            <th> Id </th>
-                            <th> Description </th>
-                            <th> Target Date </th>
-                            <th> Is Completed </th>
-                            
-                        </tr>
-                    </thead>
 
-                    <tbody>
-                        {
-                            this.state.todos.map((todo) => 
-                                <tr key={todo.id}>
-                                    <td> {todo.id} </td>
-                                    <td> {todo.description} </td>
-                                    <td> {todo.targetDate} </td>
-                                    <td> {todo.done.toString()} </td>
-                                </tr>
-                            )
-                        }
-                    </tbody>
-                </table>
+                <div className="container">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th> Id </th>
+                                <th> Description </th>
+                                <th> Target Date </th>
+                                <th> Is Completed </th>
+                                
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            {
+                                this.state.todos.map((todo) => 
+                                    <tr key={todo.id}>
+                                        <td> {todo.id} </td>
+                                        <td> {todo.description} </td>
+                                        <td> {todo.targetDate} </td>
+                                        <td> {todo.done.toString()} </td>
+                                    </tr>
+                                )
+                            }
+                        </tbody>
+                    </table>
+                </div>
+
             </div>
         );        
     }
