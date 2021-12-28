6a153cfca2faccc74e9decb3859121f5f14aed66
JianghuaiL <77869949+DN41-JH@users.noreply.github.com>
2021-12-28 16:46:22 -0600

    Update MenuBar.jsx

--- todomanagement_frontend/src/components/MenuBar.jsx
+++ todomanagement_frontend/src/components/MenuBar.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
+import { withRouter } from 'react-router';
 import { Link } from 'react-router-dom';
 import { AuthenticationService } from '../services/AuthenticationService';
   
-export default class MenuBar extends React.Component {
-    handleLogOut() {
-        window.sessionStorage.removeItem("Username");
-        window.location.href='/logout';
-    }
-
+class MenuBar extends React.Component {
     render() {
         const isLoggedIn = AuthenticationService.isLoggedIn();
 
@@ -24,11 +20,12 @@ export default class MenuBar extends React.Component {
 
                     <ul className="navbar-nav navbar-collapse justify-content-end">
                         {!isLoggedIn ? <li> <Link className="nav-link" to="/login"> Login </Link> </li> : null}
-                        {isLoggedIn ? <li> <Link className="nav-link" onClick={this.handleLogOut} to="/logout"> Logout </Link> </li> : null}
+                        {isLoggedIn ? <li> <Link className="nav-link" onClick={() => window.sessionStorage.removeItem("Username")} to="/logout"> Logout </Link> </li> : null}
                     </ul>
                 </nav>
             </header>
         );        
     }
-
 };
+
+export default withRouter(MenuBar);
