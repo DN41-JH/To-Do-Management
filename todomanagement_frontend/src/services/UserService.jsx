3219832f59f38a0e3411f401fa7bbcf1331d2095
JianghuaiL <77869949+DN41-JH@users.noreply.github.com>
2021-12-29 17:50:01 -0600

    Update Service

--- todomanagement_frontend/src/services/UserService.jsx
+++ todomanagement_frontend/src/services/UserService.jsx
@@ -1,14 +1,8 @@
 import axios from "../axios";
 
 export const UserService = {
-    login: function(loginInfo) {
-        const loginRequest = {
-            Username: loginInfo.Username,
-            Password: loginInfo.Password,
-        }
-
+    getToDos: function(username) {
         // (Must follow the EndPoint URL as specified in the backend url pattern)
-        // return axios.post('/checkStatus/', checkStatusRequest);
-        return true;
+        return axios.get(`/users/${username}/todos`);
     },
 }
