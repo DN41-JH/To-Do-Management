103eb71fb9921301acced2faa50b7f9033f4742c
JianghuaiL <77869949+DN41-JH@users.noreply.github.com>
2022-01-04 18:10:26 -0600

    Update Login.jsx

--- todomanagement_frontend/src/views/Login.jsx
+++ todomanagement_frontend/src/views/Login.jsx
@@ -40,8 +40,8 @@ export default class Login extends React.Component {
                 <h1> Login </h1>
                 <div className="container">
                     {this.state.isLoginFailed && <div className="warning"> Invalid Credential </div>}
-                    User Name: <input type='text' name='username' onChange={this.handleChange}/>
-                    Password: <input type='password' name='password' onChange={this.handleChange}/>
+                    User Name: <input type='text' name='username' style={{ marginRight: '1.5rem' }} onChange={this.handleChange}/>
+                    Password: <input type='password' name='password' style={{ marginRight: '1.5rem' }} onChange={this.handleChange}/>
                     <button className="btn btn-success" onClick={this.handleLogin}> Login </button>
                 </div>
             </div>
